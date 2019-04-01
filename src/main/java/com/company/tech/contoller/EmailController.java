ba7bfc5ae88042d2f7fea940c508a7cc4fa4971a
Sachith <tharakakk@gmail.com>
2019-04-01 13:15:40 +0530

    create email send method

---
+++ src/main/java/com/company/tech/contoller/EmailController.java
@@ -0,0 +1,38 @@
+package com.company.tech.contoller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.company.tech.request.EmailRequest;
+import com.company.tech.response.TechResponse;
+
+@RestController
+@RequestMapping("/notification")
+public class EmailController {
+	
+	 @Autowired
+	 public JavaMailSender emailSender;
+	 
+	 @RequestMapping(method = RequestMethod.POST , value="/sendEmail")
+	 public TechResponse sendEmail(@RequestBody EmailRequest emailRequest) {
+		 
+		 SimpleMailMessage message = new SimpleMailMessage();
+		  message.setTo(emailRequest.getReceiverEmail());
+	      message.setSubject("Test Simple Email");
+	      message.setText("Hello, Im testing Simple Email");
+	      
+	      this.emailSender.send(message);
+	      
+	      TechResponse response = new TechResponse();
+	      
+	      response.setResponseCode("111");
+	      response.setResponseObject("Email Send Succefully.");
+		 
+		 return response;
+	 }
+}
